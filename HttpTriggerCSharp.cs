8efdeb23ae50c606f6fe4295ee71531cf26c0a40
David Peterson <dapeters@microsoft.com>
2019-11-22 09:00:24 -0600

    Reverted Hello message

--- HttpTriggerCSharp.cs
+++ HttpTriggerCSharp.cs
@@ -26,7 +26,7 @@ namespace My.Function
             name = name ?? data?.name;
 
             return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, hi, {name}")
+                ? (ActionResult)new OkObjectResult($"Hello, {name}")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body please");
         }
     }
